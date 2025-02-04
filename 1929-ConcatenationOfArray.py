3c3393fe07fc186db2b7b4b67da1075ad76ee16e
bryryann <186111776+bryryann@users.noreply.github.com>
2025-02-04 10:21:14 -0300

    solved: 1929

---
+++ 1929-ConcatenationOfArray.py
@@ -0,0 +1,9 @@
+# Easy
+# Tags: array, simulation
+
+def get_concatenation(nums: list[int]) -> list[int]:
+    return nums + nums # yeah.....
+
+
+print(get_concatenation([1, 2, 1])) # [1, 2, 1, 1, 2, 1]
+print(get_concatenation([1, 3, 2, 1])) # [1, 3, 2, 1, 1, 3, 2, 1]
