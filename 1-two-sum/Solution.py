87a6d1ffc74f509de1c53f98238ddc843cab3e28
bryryann <186111776+bryryann@users.noreply.github.com>
2025-02-11 20:04:44 -0300

    solved: 1

---
+++ 1-two-sum/Solution.py
@@ -0,0 +1,13 @@
+# Easy
+
+def twoSum(nums: list[int], target: int) -> list[int]:
+    for i in range(len(nums)-1):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return []
+
+
+print(twoSum([2, 7, 11, 15], 9)) # [0, 1]
+print(twoSum([3, 2, 4], 6)) # [1, 2]
+print(twoSum([3, 3], 6)) # [0, 1]
